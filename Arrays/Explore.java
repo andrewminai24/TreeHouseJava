7cb7230e44e5a6db93874e9c9d1fbbe57d4c8819
andrewminai24 <andrewminai24@gmail.com>
2019-07-16 00:07:43 -0700

    arrays

---
+++ Arrays/Explore.java
@@ -0,0 +1,16 @@
+class Explore {
+
+    public static void main(String[] args) {
+        // create a new friends array using the array literal shortcut
+        // Include ben and alena pasan
+
+        String[] friends = { "BEN", "ALEN", "PASON" };
+        // Enhanced for loop
+        for (String friend : friends) {
+            System.out.printf("Hey %s! The movie starts at 7, cu there! %n", friend);
+
+        }
+
+    }
+
+}
