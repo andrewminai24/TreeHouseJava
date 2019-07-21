b7452f021dd9547e8090cb482bfd550736e7128f
andrewminai24 <andrewminai24@gmail.com>
2019-07-20 18:15:18 -0700

    strings

--- strings/Ex1.java
+++ strings/Ex1.java
@@ -1,5 +1,8 @@
 class Ex1 {
     public static void main(String[] args) {
         System.out.println("Hello \"java\"");
+        System.out.println("Use \\, not /");
+        System.out.println("1\titem");
+        System.out.println("line 1\nline 2");
     }
 }
