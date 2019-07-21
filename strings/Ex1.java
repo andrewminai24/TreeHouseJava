d3023b127326a8457d82cc5db0380fe15207c0f4
andrewminai24 <andrewminai24@gmail.com>
2019-07-20 18:16:57 -0700

    strings

--- strings/Ex1.java
+++ strings/Ex1.java
@@ -1,3 +1,9 @@
+/*
+System refers to the computer as a whole
+out refers to the standard output device the console
+println is printing a complete line of text.
+*/
+
 class Ex1 {
     public static void main(String[] args) {
         System.out.println("Hello \"java\"");
